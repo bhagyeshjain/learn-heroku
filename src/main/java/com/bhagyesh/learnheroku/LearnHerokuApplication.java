00d6da5263e6d175dbd3c6d328e75f4fce31e078
Bhagyesh Jain <bhagyesh.jain@metacube.com>
2018-05-15 13:47:40 +0530

    Initial Spring Boot Project Setup

---
+++ src/main/java/com/bhagyesh/learnheroku/LearnHerokuApplication.java
@@ -0,0 +1,12 @@
+package com.bhagyesh.learnheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnHerokuApplication.class, args);
+	}
+}
